90f1ca115d12690189081fe2be50a358a6db82af
Harshith-reddy1629 <reddybharshith3@gmail.com>
2024-05-27 12:44:23 +0530

    btn2

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -8,47 +8,45 @@ const Button = ({
   Icon = FaArrowRight,
   height = 55,
   width = 180,
-  background,
+  background = "#141414",
 }) => {
   return (
     <>
-      <button className="p-0.5 overflow-hidden inline shadow-2xl shadow-[#3a2744] hover:shadow-[#4b2774] rounded-[18px]  group hover:animate-animationOnHover animate-animationOnLeave bg-btn-gradient-rotation">
-        <span
-          style={{
-            "--height": height + "px",
-            "--width": width + "px",
-            "--button-bg": background,
-          }}
-          className="overflow-hidden skew-x-2 group-hover:skew-x-0 text-white relative rounded-[16px] w-[var(--width)] h-[var(--height)] grid grid-cols-[repeat(100,1%)] grid-rows-[repeat(2,50%)] bg-[#141316] "
-        >
-          {colorsList.map((k, i) => (
-            <span
-              style={{
-                "--shadow-color": k + "60",
-                "--span-background": k + "50",
-              }}
-              key={k}
-              className="w-full z-10 h-full hover:bg-[var(--span-background)] hover:shadow-btnSpanShadow"
-            ></span>
-          ))}
-          {colorsList.map((k, i) => (
-            <span
-              style={{
-                "--shadow-color": k + "60",
-                "--span-background": k + "50",
-              }}
-              key={k}
-              className="w-full z-10 h-full  hover:bg-[var(--span-background)] hover:shadow-btnSpanShadow"
-            ></span>
-          ))}
+      <div className="w-72 h-44 flex justify-center items-center bg-[#f4f4f4]">
+        <button className="p-0.5 overflow-hidden inline shadow-2xl shadow-[#3a2744] hover:shadow-[#4b2774] rounded-[18px]  group hover:animate-animationOnHover animate-animationOnLeave bg-btn-gradient-rotation">
+          <span
+            style={{ "--height": height + "px", "--width": width + "px" }}
+            className="overflow-hidden skew-x-2 group-hover:skew-x-0 text-black relative rounded-[16px] w-[var(--width)] h-[var(--height)] grid grid-cols-[repeat(100,1%)] grid-rows-[repeat(2,50%)] bg-[#d9d9d9] "
+          >
+            {colorsList.map((k, i) => (
+              <span
+                style={{
+                  "--shadow-color": k + "60",
+                  "--span-background": k + "50",
+                }}
+                key={k}
+                className="w-full z-10 h-full hover:bg-[var(--span-background)] hover:shadow-btnSpanShadow"
+              ></span>
+            ))}
+            {colorsList.map((k, i) => (
+              <span
+                style={{
+                  "--shadow-color": k + "60",
+                  "--span-background": k + "50",
+                }}
+                key={k}
+                className="w-full z-10 h-full  hover:bg-[var(--span-background)] hover:shadow-btnSpanShadow"
+              ></span>
+            ))}
 
-          <span className="-skew-x-2 absolute w-full justify-center px-1  flex gap-2 items-center  group-hover:skew-x-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            {" "}
-            {text}
-            <Icon className="group-hover:translate-x-1.5 text-sm transition duration-500 " />
+            <span className="-skew-x-2 absolute w-full justify-center px-1  flex gap-2 items-center  group-hover:skew-x-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              {" "}
+              {text}
+              <Icon className="group-hover:translate-x-1.5 text-sm transition duration-500 " />
+            </span>
           </span>
-        </span>
-      </button>
+        </button>
+      </div>
     </>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -12,43 +12,43 @@ function Home({
 }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between  p-24 bg-[#1c1c1c]">
-      <Button id={1} text="anime" />
+      <button className="p-0.5 overflow-hidden inline shadow-2xl z-0 shadow-[#3a2744] hover:shadow-[#4b2774] rounded-[18px]  group hover:animate-animationOnHover animate-animationOnLeave bg-btn-gradient-rotation">
+        <span
+          style={{
+            "--height": height + "px",
+            "--width": width + "px",
+          }}
+          className="!bg-[#141414] overflow-hidden text-white relative rounded-[16px] w-[var(--width)] h-[var(--height)] grid grid-cols-[repeat(100,1%)] grid-rows-[repeat(2,50%)] "
+        >
+          {colorsList.map((k, i) => (
+            <span
+              style={{
+                "--shadow-color": k + "60",
+                "--span-background": k + "50",
+              }}
+              key={k}
+              className="w-full z-10 h-full hover:bg-[var(--span-background)] hover:shadow-btnSpanShadow"
+            ></span>
+          ))}
+          {colorsList.map((k, i) => (
+            <span
+              style={{
+                "--shadow-color": k + "60",
+                "--span-background": k + "50",
+              }}
+              key={k}
+              className="w-full z-10 h-full  hover:bg-[var(--span-background)] hover:shadow-btnSpanShadow"
+            ></span>
+          ))}
 
-      <div className="w-72 h-44 flex justify-center items-center bg-[#f4f4f4]">
-        <button className="p-0.5 overflow-hidden inline shadow-2xl shadow-[#3a2744] hover:shadow-[#4b2774] rounded-[18px]  group hover:animate-animationOnHover animate-animationOnLeave bg-btn-gradient-rotation">
-          <span
-            style={{ "--height": height + "px", "--width": width + "px" }}
-            className="overflow-hidden skew-x-2 group-hover:skew-x-0 text-black relative rounded-[16px] w-[var(--width)] h-[var(--height)] grid grid-cols-[repeat(100,1%)] grid-rows-[repeat(2,50%)] bg-[#d9d9d9] "
-          >
-            {colorsList.map((k, i) => (
-              <span
-                style={{
-                  "--shadow-color": k + "60",
-                  "--span-background": k + "50",
-                }}
-                key={k}
-                className="w-full z-10 h-full hover:bg-[var(--span-background)] hover:shadow-btnSpanShadow"
-              ></span>
-            ))}
-            {colorsList.map((k, i) => (
-              <span
-                style={{
-                  "--shadow-color": k + "60",
-                  "--span-background": k + "50",
-                }}
-                key={k}
-                className="w-full z-10 h-full  hover:bg-[var(--span-background)] hover:shadow-btnSpanShadow"
-              ></span>
-            ))}
-
-            <span className="-skew-x-2 absolute w-full justify-center px-1  flex gap-2 items-center  group-hover:skew-x-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-              {" "}
-              {text}
-              <Icon className="group-hover:translate-x-1.5 text-sm transition duration-500 " />
-            </span>
+          <span className="-skew-x-2 absolute w-full justify-center px-1  flex gap-2 items-center  group-hover:skew-x-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            {" "}
+            {text}
+            <Icon className="group-hover:translate-x-1.5 text-sm transition duration-500 " />
           </span>
-        </button>
-      </div>
+        </span>
+      </button>
+      <Button id={1} text="anime" />
     </main>
   );
 }
