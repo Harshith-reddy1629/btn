b0171b466373347613562d4ca4d2bae9a62b4e54
Harshith-reddy1629 <reddybharshith3@gmail.com>
2024-05-27 11:58:20 +0530

    btn

---
+++ src/app/components/Button.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import colorsList from "../constant.json";
+import { FaArrowRight } from "react-icons/fa";
+
+const Button = ({
+  id,
+  text = "Button",
+  Icon = FaArrowRight,
+  height = 55,
+  width = 180,
+  background,
+}) => {
+  return (
+    <>
+      <div className="p-0.5 overflow-hidden inline shadow-2xl shadow-[#3a2744] hover:shadow-[#4b2774] rounded-[18px]  group hover:animate-animationOnHover animate-animationOnLeave bg-btn-gradient-rotation">
+        <button
+          style={{
+            "--height": height + "px",
+            "--width": width + "px",
+            "--button-bg": background,
+          }}
+          className="overflow-hidden skew-x-2 group-hover:skew-x-0 text-white relative rounded-[16px] w-[var(--width)] h-[var(--height)] grid grid-cols-[repeat(100,1%)] grid-rows-[repeat(2,50%)] bg-[#141316] "
+        >
+          {colorsList.map((k, i) => (
+            <span
+              style={{
+                "--shadow-color": k + "60",
+                "--span-background": k + "50",
+              }}
+              key={k}
+              className="w-full z-10 h-full hover:bg-[var(--span-background)] hover:shadow-btnSpanShadow"
+            ></span>
+          ))}
+          {colorsList.map((k, i) => (
+            <span
+              style={{
+                "--shadow-color": k + "60",
+                "--span-background": k + "50",
+              }}
+              key={k}
+              className="w-full z-10 h-full  hover:bg-[var(--span-background)] hover:shadow-btnSpanShadow"
+            ></span>
+          ))}
+
+          <span className="-skew-x-2 absolute w-full justify-center px-1  flex gap-2 items-center  group-hover:skew-x-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            {" "}
+            {text}
+            <Icon className="group-hover:translate-x-1.5 text-sm transition duration-500 " />
+          </span>
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages//*.{js,ts,jsx,tsx,mdx}",
+    "./src/components//*.{js,ts,jsx,tsx,mdx}",
+    "./src/app//*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      boxShadow: {
+        btnSpanShadow: "0 0 35px 26px var(--shadow-color)",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "btn-gradient-rotation":
+          "linear-gradient( var(--gradient-angle) , #0D6EFD ,#ffffff40, #c0c0c0)",
+      },
+      backgroundSize: {
+        "95%": "calc(100% - 5px) calc(100% - 5px)",
+        "100%": "100% 100%",
+      },
+      animation: {
+        animationOnLeave: "animationOnLeave 1.3s forwards",
+        animationOnHover: "animationOnHover .75s forwards",
+      },
+      keyframes: {
+        animationOnLeave: {
+          "0%": {
+            "--gradient-angle": "225deg",
+          },
+          "100%": {
+            "--gradient-angle": "45deg",
+          },
+        },
+        animationOnHover: {
+          "0%": {
+            "--gradient-angle": "45deg",
+          },
+          "100%": {
+            "--gradient-angle": "225deg",
+          },
+        },
       },
     },
   },
